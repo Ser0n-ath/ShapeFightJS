12d6f800e9e8d53b864c2887644809c3909d134b
Ser0n-ath <seron.ath@gmail.com>
2021-12-31 21:33:46 -0500

    add game loop

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 var canvas = document.getElementById("game-container");
 var ctx = canvas.getContext("2d");
 
+var x = 0;
+var y = 0;
 
 //Test
 window.addEventListener('load', () => {
@@ -16,11 +18,16 @@ window.addEventListener('resize', () => {
     draw();
 }, false);
 
+
 function draw(){
-    ctx.beginPath();
-    ctx.rect(50, 50, 50, 50);
-    ctx.fillStyle = "#FF0000";
-    ctx.fill();
-    ctx.closePath();
-    
-}//Draw code
+ctx.clearRect(0,0,canvas.width, canvas.height);
+ctx.beginPath();
+ctx.rect(x, y, 50, 50);
+ctx.fillStyle = "#FF0000";
+ctx.fill();
+ctx.closePath();
+    x+= 5;
+    y+= 5;
+}
+
+setInterval(draw, 16);
