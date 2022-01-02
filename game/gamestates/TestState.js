9a0212c904e16a7159857455d72ec3908a86229a
Ser0n-ath <seron.ath@gmail.com>
2022-01-01 23:37:26 -0500

    Add input controllers & mediator for I/O

--- game/gamestates/TestState.js
+++ game/gamestates/TestState.js
@@ -1,18 +1,42 @@
 export class TestState{
+
+    #controlResults;
+
     constructor(){
         this.x = 50;
         this.y = 50;
     }
 
-    draw(canvas, ctx){
+
+    KeyEventManager(controlResults){
+        var rightPressed, leftPressed, downPressed, upPressed;
+        [rightPressed, leftPressed, upPressed, downPressed] = controlResults; //deconstructing
+
+        if(rightPressed) {
+            this.x += 5;
+        }
+        else if(leftPressed) {
+            this.x -= 5;
+        }
+        if(downPressed) {
+            this.y += 5;
+        }
+        else if(upPressed) {
+            this.y -= 5;
+        }
+    }
+
+    draw(canvas, ctx, controlResults){
         ctx.clearRect(0,0,canvas.width, canvas.height);
         ctx.beginPath();
         ctx.rect(this.x, this.y, 50, 50);
         ctx.fillStyle = "#FF0000";
         ctx.fill();
         ctx.closePath();
+        this.KeyEventManager(this.#controlResults);
+    }
 
-        this.x+= 1
-        this.y+= 1;
+    ISCreadStatus(ReadISC_Controller){
+        this.#controlResults = ReadISC_Controller;
     }
 }
