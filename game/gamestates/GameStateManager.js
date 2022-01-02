bf4f8a6fbdedd68dd85669e38835ff41f42718fa
Ser0n-ath <seron.ath@gmail.com>
2022-01-01 23:38:58 -0500

    Added input controls & mediator for I/O

--- game/gamestates/GameStateManager.js
+++ game/gamestates/GameStateManager.js
@@ -1,32 +1,39 @@
 export class GameStateManager{
+    #states = [] //Stack of states, ie menu->game mode-> map ->level 
+    #controller; //the controller the user is using to interact with the game
 
-    #states = []
-
-    constructor(gameState){
+    constructor(gameState, controls){
         this.#states.push(gameState);
+        this.#controller = controls;
     }
 
     newState(newState){
-        this.#states.push(newState)
+        this.#states.push(newState); 
     }
 
-    //returns the current state and reverts to previous state
     endCurrentState(){
-
-        if(this.#states.length === 1){ //We at the main menu 
+        if(this.#states.length === 1){ 
             return null;
         }else{
             return this.#states.pop();
         }
     }
 
-    //Draws the state at the top of the stack.
+    //Shares information between the game state/controllers/display/sound
+    interCommunicate(currState){
+        var gameController =  this.#controller;
+        currState.ISCreadStatus(gameController.ISCgetStatus());
+     }
+
+    //Todo: move functions to the display js.
     draw(canvas, ctx){
         var currState = this.#states[this.#states.length-1];
+        this.interCommunicate(currState);
         currState.draw(canvas,ctx);
+
     }
 
-    //Return the current state of the game.
+    //Returns the current state (peek fxn in stack datastructure)
     getCurrentState(){
         var currState = this.#states[this.#states.length-1];
         return currState;
